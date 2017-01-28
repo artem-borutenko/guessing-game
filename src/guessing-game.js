a404afbca39117ee3bc76cb2a612b0e50d069886
Artem Borutenko <artem.borutenko@gmail.com>
2017-01-28 18:08:19 +0300

    all

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,34 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+		this.min=0;
+		this.max=0
+	    this.current=0;
+	
+	
+	
+	}
     setRange(min, max) {
+		this.min=min;
+		this.max=max;
 
     }
 
     guess() {
-
-    }
+		
+			this.current=Math.ceil((this.min+this.max)/2);
+			return this.current;
+	}
+			
 
     lower() {
-
-    }
-
+		this.max = this.current ;
+		return this.max;
+			
+			}
     greater() {
-
+		this.min = this.current;
+		return this.min;
+		
     }
 }
 
